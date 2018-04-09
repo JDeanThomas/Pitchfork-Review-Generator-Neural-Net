4b363a7a300098b24b39152524a57adf54175feb
JDeanThomas <JDeanThomas#gmail.com>
2018-04-09 14:02:36 -0400

    Starting project

--- word2vec.py
+++ word2vec.py
@@ -1,10 +1,12 @@
 import gensim
 from gensim.models import word2vec
+import logging
 import re
+import numpy as np
 
 def read_data(filename):
-    with filename as f:
-        data = f.read(f.namelist()[0]).split()
+    with open(filename, "r") as f:
+        data = f.read().split()
     return data
 
 def convert_data_to_index(string_data, wv):
@@ -22,17 +24,18 @@ def convert_data_to_index(string_data, wv):
 
 sentences = word2vec.LineSentence('pitchfork.txt')
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
 model = word2vec.Word2Vec(sentences, size=300, window=5, min_count=2, iter=10, sg=1,
-                            negative=True, compute_loss=True, workers=8)
+                                    negative=True, compute_loss=True, workers=8)
 
 model.save("pitch2vec")
 
 str_data = read_data("pitchfork.txt")
 index_data = convert_data_to_index(str_data, model.wv)
 
-
 # convert the wv word vectors into a numpy matrix
-embedding_matrix = np.zeros((len(model.wv.vocab), vector_dim))
+embedding_matrix = np.zeros((len(model.wv.vocab), 300))
 for i in range(len(model.wv.vocab)):
     embedding_vector = model.wv[model.wv.index2word[i]]
     if embedding_vector is not None:
