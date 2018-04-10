050973096fc4fa6388191e208a5eda8d7d951978
JDeanThomas <JDeanThomas#gmail.com>
2018-04-10 04:15:07 -0400

    Starting project

--- readDBextract.py
+++ readDBextract.py
@@ -1,10 +1,12 @@
 import zipfile
 import sqlite3
+import unicodedata
 
 zipfile.ZipFile("database.sqlite.zip", 'r').extractall()
 zip_ref.close()
 
 conn = sqlite3.connect("database.sqlite")
+conn.text_factory = str
 cur = conn.cursor()
 
 cur.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
@@ -14,6 +16,16 @@ cur.execute("SELECT content FROM content")
 pitchfork = cur.fetchall()
 print(pitchfork[0])
 conn.close()
+
+
+def normalize_unicode(query):
+    data = []
+    for row in query:
+        data.append(unicodedata.normalize("NFKD", row[0]))
+    return data
+
+pitchfork = normalize_unicode(pitchfork)
+
 
 file = open('pitchfork.txt', 'w')
 for review in pitchfork:
--- word2vec.py
+++ word2vec.py
@@ -4,6 +4,7 @@ import logging
 import re
 import numpy as np
 
+
 def read_data(filename):
     with open(filename, "r") as f:
         data = f.read().split()
@@ -17,19 +18,31 @@ def convert_data_to_index(string_data, wv):
     return index_data
 
 #with open("pitchfork.txt") as f:
-    #text = f.read()
+    #text = f.readlines()
     #text = re.sub(r'(M\w{1,2})\.', r'\1', text)
+    #text = re.split(r' *[\.\?!][\'"\)\]]* *', text)
 
-#sentences = re.split(r' *[\.\?!][\'"\)\]]* *', text)
 
 sentences = word2vec.LineSentence('pitchfork.txt')
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-model = word2vec.Word2Vec(sentences, size=300, window=5, min_count=2, iter=10, sg=1,
-                                    negative=True, compute_loss=True, workers=8)
+model = word2vec.Word2Vec(sentences, size=300, window=5, min_count=2, iter=10,
+                                     sample=0.00001, sg=1, negative=20,
+                                     compute_loss=True, workers=8)
 
 model.save("pitch2vec")
+
+vocab_size = len(model.wv.vocab)
+print(model.wv.index2word[0], model.wv.index2word[1], model.wv.index2word[2])
+print(model.wv.index2word[vocab_size - 1], model.wv.index2word[vocab_size - 2],
+      model.wv.index2word[vocab_size - 3])
+print('Index of "of" is: {}'.format(model.wv.vocab['of'].index))
+# Similarity
+print(model.wv.similarity('woman', 'man'), model.wv.similarity('man', 'elephant'))
+# what doesn't fit?
+print(model.wv.doesnt_match("green blue red zebra".split()))
+
 
 str_data = read_data("pitchfork.txt")
 index_data = convert_data_to_index(str_data, model.wv)
